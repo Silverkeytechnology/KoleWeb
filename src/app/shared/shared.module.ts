c6a69f6aaf6f1b29218f4598fef6068af33d8216
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-29 09:44:35 +0200

    Fix invalid directory refs for components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NavigationHeaderComponent } from './shared/navigation-header/navigation-header.component';
-import { SideMenuComponent } from './shared/side-menu/side-menu.component';
-import { PageFooterComponent } from './shared/page-footer/page-footer.component';
-import { MainContentComponent } from './shared/main-content/main-content.component';
+import { NavigationHeaderComponent } from './navigation-header/navigation-header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   imports: [
