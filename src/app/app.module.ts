6bba4c69814f2b9825ffc70c2c50721a66ce14af
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-29 09:09:27 +0200

    Stub out shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationHeaderComponent } from './shared/navigation-header/navigation-header.component';
+import { SideMenuComponent } from './shared/side-menu/side-menu.component';
+import { PageFooterComponent } from './shared/page-footer/page-footer.component';
+import { MainContentComponent } from './shared/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationHeaderComponent,
+    SideMenuComponent,
+    PageFooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
