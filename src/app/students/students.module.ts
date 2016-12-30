79037807fc5f757ca0a3335ecbe04009c331206e
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-30 11:26:41 +0200

    Add students component stub

--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { StudentsListComponent } from './students-list/students-list.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
 import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StudentsListComponent, StudentDetailComponent, StudentFormComponent]
+  declarations: [StudentsListComponent, StudentDetailComponent, StudentFormComponent, StudentsComponent]
 })
 export class StudentsModule { }
