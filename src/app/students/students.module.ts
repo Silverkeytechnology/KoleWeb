1f34a7b51f1a3472f7767f59216e5a67b750c71d
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-31 21:56:31 +0200

    Cleanups

--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgLocalization } from '@angular/common';
 import { StudentsListComponent } from './students-list/students-list.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
 import { StudentFormComponent } from './student-form/student-form.component';
-import { StudentsComponent } from './students/students.component';
+import { StudentsComponent } from './students.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StudentsListComponent, StudentDetailComponent, StudentFormComponent, StudentsComponent]
+  declarations: [StudentsListComponent, StudentDetailComponent, StudentFormComponent, StudentsComponent],
+  exports: [StudentsListComponent,StudentDetailComponent,StudentFormComponent,StudentFormComponent,StudentsComponent ]
 })
 export class StudentsModule { }
