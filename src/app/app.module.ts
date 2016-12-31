e5b66fec40fcb1ee56dada127504c9b863c45c63
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-12-31 22:54:05 +0200

    Configure router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { StudentsModule } from './students/students.module';
 
+//Setup Routing
+const appRoutes: Routes = [
+  { path: 'students-center', component: StudentsComponent},
+  { path: 'students-list', component: StudentsListComponent},
+  { path: 'student/:id', component: StudentDetailComponent},
+  { path: '',
+    redirectTo: '/students',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent}
+];
+
+RouterModule.forRoot([
+  {
+    path: 'students',
+    component: StudentsComponent
+  }
+])
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
