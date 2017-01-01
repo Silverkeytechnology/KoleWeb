f113b60d97c6791384566b289b60e850f261aec1
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-02 00:17:48 +0200

    Add routes for dashboards

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,14 +2,20 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { StudentsComponent } from './students/students.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { StudentsDashboardComponent } from './students/students-dashboard/students-dashboard.component';
+import { ClassesDashboardComponent } from './students/classes-dashboard/classes-dashboard.component';
+import { CoursesDashboardComponent } from './courses/courses-dashboard/courses-dashboard.component';
+import { InstructorsDashboardComponent } from './instructors/instructors-dashboard/instructors-dashboard.component';
+import { SchoolsDashboardComponent } from './schools/schools-dashboard/schools-dashboard.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: 'students-center', component: StudentsComponent, data: {title: 'Kole Students Center!'}},
-  { path: 'students', component: StudentsListComponent, data: {title: 'Kole Students'}},
-
+  { path: 'students-dashboard', component: StudentsDashboardComponent, data: {title: 'Kole Students Center!'}},
+  { path: 'classes-dashboard', component: ClassesDashboardComponent, data: {title: 'Kole Classes Center!'}},
+  { path: 'courses-dashboard', component: CoursesDashboardComponent, data: {title: 'Kole Courses Center!'}},
+  { path: 'instructors-dashboard', component: InstructorsDashboardComponent, data: {title: 'Kole Instructors Center!'}},
+  { path: 'schools-dashboard', component: SchoolsDashboardComponent, data: {title: 'Kole Schools Center!'}},
+  
   { path: '', redirectTo: '/students-center', pathMatch: 'full', data: {title: 'Kole Students Center!'}},
   { path: '**', component: PageNotFoundComponent, data: {title: 'Page Not Found'}}
 ];
