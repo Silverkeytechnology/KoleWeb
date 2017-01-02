e394114e0626f90925ff3c40ffe7bad4f97ef1a7
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-02 14:28:49 +0200

    Add compose message

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ComposeMessageComponent
   ],
   exports: [
     PageNotFoundComponent]
