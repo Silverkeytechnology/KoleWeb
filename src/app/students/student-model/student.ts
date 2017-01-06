3df5474b44868699571f3ab6b179260250f9a627
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-06 20:54:00 +0200

    Add student model

---
+++ src/app/students/student-model/student.ts
@@ -0,0 +1,11 @@
+export class Student {
+  firstName: string;
+  lastName: string;
+  gender: string;
+
+  constructor(fName: string, lName: string, sex: string){
+    this.firstName = fName;
+    this.lastName = lName;
+    this.gender = sex;
+  }
+}
