be2f8efa7cf09a04b28de35f0561fd967ff809fe
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-07 22:51:30 +0200

    Add fuelux

--- src/app/students/students-router/students-router.module.ts
+++ src/app/students/students-router/students-router.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentsDashboardComponent } from '../students-dashboard/students-dashboard.component';
@@ -5,6 +6,7 @@ import { StudentsListComponent } from '../students-list/students-list.component'
 import { StudentDetailComponent } from '../student-detail/student-detail.component';
 import { StudentsCenterComponent } from '../students-center/students-center.component';
 import { StudentFormComponent } from '../student-form/student-form.component';
+import { StudentsEnrollmentComponent } from '../students-enrollment/students-enrollment.component';
 
 const studentsRoutes: Routes = [
 
@@ -12,9 +14,14 @@ const studentsRoutes: Routes = [
       children: [
         { path: '', redirectTo: '/students-center/students-dashboard', pathMatch: 'full' },
         { path: 'students-dashboard',component: StudentsDashboardComponent },
-        { path: 'students-list',component: StudentsListComponent,
+        { path: 'students-enrollment',component: StudentsEnrollmentComponent,
           children: [
             { path: 'new', component: StudentFormComponent },
+            { path: ':id', component: StudentDetailComponent }
+          ]
+        },
+        { path: 'students-list',component: StudentsListComponent,
+          children: [
             { path: ':id', component: StudentDetailComponent }
           ]
         }
