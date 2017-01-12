b8c90d8f9d40eba74b7f672f6e0fec24f8e9cf3c
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-12 21:04:29 +0200

    Add health profile'

--- src/app/students/student-profile/student-profile.module.ts
+++ src/app/students/student-profile/student-profile.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { StudentProfileComponent } from './student-profile/student-profile.component';
 import { AcademicProfileComponent } from './academic-profile/academic-profile.component';
 import { DisciplinaryProfileComponent } from './disciplinary-profile/disciplinary-profile.component';
+import { HealthProfileComponent } from './health-profile/health-profile.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StudentProfileComponent, AcademicProfileComponent, DisciplinaryProfileComponent]
+  declarations: [StudentProfileComponent, AcademicProfileComponent, DisciplinaryProfileComponent, HealthProfileComponent]
 })
 export class StudentProfileModule { }
