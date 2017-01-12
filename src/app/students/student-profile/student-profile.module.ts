5c5279525cd2efba5e5b6f4acd5536ab1257d41a
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-12 21:01:15 +0200

    Add academic profile component

--- src/app/students/student-profile/student-profile.module.ts
+++ src/app/students/student-profile/student-profile.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { AcademicProfileComponent } from './academic-profile/academic-profile.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StudentProfileComponent]
+  declarations: [StudentProfileComponent, AcademicProfileComponent]
 })
 export class StudentProfileModule { }
