155ecc11ab0eaa37dd2b2e76349b6699f8a08a7e
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-01-15 10:56:48 +0200

    Setup student module

--- src/app/students/student/student.module.ts
+++ src/app/students/student/student.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StudentProfileRouterModule } from './student-router/student-router.module';
+import { StudentAttendanceComponent } from './student-attendance/student-attendance.component';
+import { StudentDisciplinaryComponent } from './student-disciplinary/student-disciplinary.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentHealthComponent } from './student-health/student-health.component';
+import { StudentPaymentsComponent } from './student-payments/student-payments.component';
+import { StudentPerformanceComponent } from './student-performance/student-performance.component';
+import { StudentPersonalInfoComponent } from './student-personal-info/student-personal-info.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { StudentSponsorshipComponent } from './student-sponsorship/student-sponsorship.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StudentProfileRouterModule
   ],
-  declarations: []
+  declarations: [StudentProfileRouterModule, StudentAttendanceComponent, StudentDisciplinaryComponent, StudentFormComponent, StudentHealthComponent, StudentPaymentsComponent, StudentPerformanceComponent, StudentPersonalInfoComponent, StudentProfileComponent, StudentSponsorshipComponent],
+  exports: [StudentProfileRouterModule, StudentAttendanceComponent, StudentDisciplinaryComponent, StudentFormComponent, StudentHealthComponent, StudentPaymentsComponent, StudentPerformanceComponent, StudentPersonalInfoComponent, StudentProfileComponent, StudentSponsorshipComponent]
 })
 export class StudentModule { }
--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -5,6 +5,7 @@ import { StudentsListComponent } from './students-list/students-list.component';
 import { StudentsDashboardComponent } from './students-dashboard/students-dashboard.component';
 import { StudentsCenterComponent } from './students-center/students-center.component';
 import { StudentsRouterModule } from './students-router/students-router.module';
+import { StudentModule } from './student/student.module';
 import { StudentsEnrollmentComponent } from './students-enrollment/students-enrollment.component';
 import { StudentsAttendanceComponent } from './students-attendance/students-attendance.component';
 import { StudentsPerformanceComponent } from './students-performance/students-performance.component';
@@ -20,10 +21,11 @@ import { StudentAttendanceComponent } from './student/student-attendance/student
   imports: [
     NgbModule,
     CommonModule,
-    StudentsRouterModule
+    StudentsRouterModule,
+    StudentModule
   ],
   declarations: [StudentsListComponent,StudentsDashboardComponent, StudentsCenterComponent, StudentsEnrollmentComponent, StudentsAttendanceComponent, StudentsPerformanceComponent, StudentsTransferComponent, StudentsDisciplinaryComponent, StudentsPaymentsComponent, StudentsSponsorshipComponent, StudentsHealthComponent, StudentAttendanceComponent],
   //entryComponents: [StudentDetailContent],
-  exports: [StudentsListComponent,StudentsDashboardComponent, StudentsCenterComponent, StudentsEnrollmentComponent, StudentsAttendanceComponent, StudentsPerformanceComponent, StudentsTransferComponent, StudentsDisciplinaryComponent, StudentsPaymentsComponent, StudentsSponsorshipComponent]
+  exports: [StudentsListComponent,StudentsDashboardComponent, StudentsCenterComponent, StudentsEnrollmentComponent, StudentsAttendanceComponent, StudentsPerformanceComponent, StudentsTransferComponent, StudentsDisciplinaryComponent, StudentsPaymentsComponent, StudentsSponsorshipComponent, StudentsHealthComponent, StudentAttendanceComponent]
 })
 export class StudentsModule { }
