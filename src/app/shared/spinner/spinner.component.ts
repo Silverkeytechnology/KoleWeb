e06be64981a37d699be5841a6c0f1f632919b01e
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-02-03 15:25:40 +0200

    remove moduleid from spinner

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { LoggerService } from '../logger/logger.service';
+import { SpinnerState } from './spinner.service';
+import { SpinnerService } from './spinner.service';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  visible = false;
+
+  private spinnerSateChanged: Subscription;
+
+  constructor(
+    private loggerService: LoggerService,
+    private spinnerService: SpinnerService
+  ) { }
 
   ngOnInit() {
+    console.log(this.visible);
+    this.spinnerSateChanged = this.spinnerService.spinnerState
+      .subscribe((state: SpinnerState) => {
+        this.visible = state.show;
+        this.loggerService.log(`visible=${this.visible}`);
+      });
+  }
+  ngOnDestroy() {
+    this.spinnerSateChanged.unsubscribe();
+
   }
 
 }
